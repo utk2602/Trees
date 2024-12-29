6bc5adfcd1159037a0102470a7dd8a822e9835eb
utk2602 <166751207+utk2602@users.noreply.github.com>
2024-12-29 13:43:55 +0530

    29-12-2024

--- hi.java
+++ hi.java
@@ -1,1 +1,30 @@
-yoiii   
+/*functions in trees are done in O(logN) times 
+unbalanced binary tree
+size = no of blocks
+siblings(nodes side by side)
+heigh - max number of edges between two node 
+
+
+
+complete Binary tree
+all level apart from the leaf will be full
+and all if the last level is full it will be filled from left to right 
+
+
+Full Binary Tree
+Each node has either zero or two children
+
+
+
+Perfect Binary Tree
+triangle \
+
+
+Height balanced tree:- average height O(logN)
+
+
+Skewed Binary tree every Node has only one childd
+
+
+Ordered Bnary Tree - Every node has a property 
+*/
